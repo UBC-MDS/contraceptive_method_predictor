63a73530698b427abe143416c735f525623573e7
Abhiket Gaurav <abhiket@gmail.com>
2021-11-25 17:20:50 -0800

    Initial_ML_Model

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,12 +1,13 @@
-# author: Christopher Alexander
+# author: Christophe Alexander
 # date: 2021-11-25
 
 """Reads data csv data from path and stores partitioned data to a local filepath as a csv.
 Usage: split_data.py --path=<path> --out_file=<out_file> 
  
 Options:
---path=<path>           Path to read file from.
---out_file=<out_file>   Path (including filename) of where to locally write the patitioned file
+--path=<path>               Path to read file from 
+--score_file=<score_file>   Path (including filename) of where to locally save cross val score
+--model_path=<model_path>   Path for the model pickle file
 """
 
 import os
@@ -18,16 +19,6 @@ from sklearn.model_selection import train_test_split
 
 opt = docopt(__doc__)
 
-columns = ["Wife_age",
-        "Wife_education",
-        "Husband_education",
-        "Number_of_children_ever_born",
-        "Wife_religion",
-        "Wife_now_working?",
-        "Husband_occupation",
-        "Standard_of_living_index",
-        "Media_exposure",
-        "Contraceptive_method_used"]
 
 def main(path, out_file):
     data = pd.read_csv(path, header=0, names=columns)
