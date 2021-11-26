9e8b206ae603b9ae356d98b80cf26c5ab701282f
Abhiket Gaurav <abhiket@gmail.com>
2021-11-25 18:43:56 -0800

    comments resolved

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,11 +1,11 @@
 # author: Abhiket Gaurav
 # date: 2021-11-25
 
-"""Reads train csv data from path preprocess the data and them build a Model on it and gives the cross validation output"
+"""Reads train csv data from path, preprocess the data, build a Model, gives the cross validation output"
 Usage: Preprocessor.py --path=<path> --score_file=<score_file> --model_path=<model_path>
  
 Options:
---path=<path>               Path to read file from 
+--path=<path>               Path to read file from
 --score_file=<score_file>   Path (including filename) of where to locally save cross val score
 --model_path=<model_path>   Path for the model pickle file
 """
@@ -52,37 +52,42 @@ opt = docopt(__doc__)
 
 
 def main(path, out_file, model_path):
+    # Reading the data
     train_df = pd.read_csv(path)
     
+    # Splitting between Features & Tar
     X_train, y_train = train_df.drop(columns=["Contraceptive_method_used"]), train_df["Contraceptive_method_used"]
 
     #Converting it to a binary model(train set)
     y_train = y_train.replace(1,0)
     y_train = y_train.replace([2,3],1)
 
+    #Cleaning and Pre-processing
     numeric_features = ['Wife_age', 'Number_of_children_ever_born']
     ordinal_features = ["Wife_education","Husband_education","Husband_occupation", "Standard_of_living_index"]
     passthrough_features = ['Wife_religion','Wife_now_working?','Media_exposure'] 
-
 
     preprocessor = make_column_transformer(
         (make_pipeline(SimpleImputer(), StandardScaler()),numeric_features,),
         (OrdinalEncoder(),ordinal_features,),  
         ("passthrough", passthrough_features),)
-    
+
+    # Building the Model
     models_bal = {
-    "decision tree": DecisionTreeClassifier(random_state=123),
-    "kNN": KNeighborsClassifier(),
-    "Logistic Regression": LogisticRegression(max_iter=100, multi_class='ovr',random_state=123 ),
-    "RBF SVM": SVC(random_state=123),}
+        "decision tree": DecisionTreeClassifier(random_state=123),
+        "kNN": KNeighborsClassifier(),
+        "Logistic Regression": LogisticRegression(max_iter=100, multi_class='ovr',random_state=123 ),
+        "RBF SVM": SVC(random_state=123),}
     
+    #Cross val-score
     results_bal = {}
     results_bal_f = {}
-
     for keys, value in models_bal.items():
         pipe_bal = make_pipeline(preprocessor, value)
         results_bal[keys] = cross_validate(pipe_bal, X_train, y_train, cv = 5, return_train_score = True)
         results_bal_f[keys] = pd.DataFrame(results_bal[keys]).mean()
+        
+    # Output: saving the cross val score in a CSV file
     
     try:
         pd.DataFrame(results_bal_f).to_csv(out_file)
@@ -90,10 +95,8 @@ def main(path, out_file, model_path):
         os.makedirs(os.path.dirname(out_file))
         pd.DataFrame(results_bal_f).to_csv(out_file)
 
-# if __name__ == "__main__":
-#     main(opt["--path"], opt["--score_file"],opt["--model_path"])
-
 if __name__ == "__main__":
-    main(opt["--path"], opt["--out_file"])
+    main(opt["--path"], opt["--score_file"],opt["--model_path"])
+
 
 
